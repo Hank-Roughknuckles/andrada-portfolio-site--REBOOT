91aed0496368d4b00e9f3272b5afe3dc95e60f5c
Thomas Morris <thomas.imorris@gmail.com>
2014-04-18 12:06:35 +0300

    DRY out controller

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,7 +1,18 @@
 class LandingPagesController < ApplicationController
   layout "landing_page_layout"
+  before_action :get_photo
+
+  before_filter :authorize, :except => :show
+  helper_method :get_slide_background_css
 
   def show
+  end
+
+  def edit
+  end
+
+private
+  def get_photo
     @landing_page_photo = LandingPagePhoto.first
   end
 end
